cfaabad609db1745fc0c36db383152e5e61a3e7d
ArmSargsyan <wwwarmyan1990@gmail.com>
2022-04-12 16:45:41 +0400

    point 6 bridge pattern

---
+++ src/patterns/bridje/Device.java
@@ -0,0 +1,14 @@
+package patterns.bridje;
+
+public interface Device {
+
+    boolean isEnabled();
+    void enable();
+    void disable();
+    int getVolume();
+    void setVolume(int percent);
+    int getChannel();
+    void setChannel(int channel);
+    void printStatus();
+
+}
